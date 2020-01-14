3776de6e07c56ab289007e3129d564352f619d15
Sam Erie <serie@alaska.edu>
2020-01-13 20:23:06 -0900

    using limited queue to keep some cams in memory

--- src/camera_pool.hpp
+++ src/camera_pool.hpp
@@ -9,6 +9,10 @@ using std::vector;
 using std::string;
 #include <unordered_map>
 using std::unordered_map;
+#include <queue>
+using std::queue;
+#include <memory>
+using std::shared_ptr;
 
 #include <opencv2/opencv.hpp>
 using namespace cv;
@@ -16,10 +20,42 @@ using namespace cv;
 
 class CameraPool
 {
+  struct Camera
+  {
+    shared_ptr<VideoCapture> cam;
+    int camId;
+    Camera(int cid)
+    {
+      camId = cid;
+      cam = std::make_shared<VideoCapture>(camId);
+    }
+
+  }
   vector<int> cameras;
+  // todo make lookup table
+  queue<Camera> openCams;
   // vector<VideoCapture> cameras;
   const int MAXCAMS = 20;
+
+  //TODO use this instead of hardcoded
+  const int MAXOPEN = 2;  // doesn't seem to be enough mem on rpi to hold 3 simultaneously
   unordered_map<int, string> lastframe;
+
+
+  shared_ptr<VideoCapture> getCam(int cid)
+  {
+    //TODO this logic only works with MAXOPEN=2
+    if(openCams.front().camId == cid)
+      return openCams.front().cam;
+    if(openCams.back().camId == cid)
+      return openCams.back().cam;
+
+    Camera camera(cid);
+    if(openCams.size() >= MAXOPEN)
+      openCams.pop();
+    openCams.emplace(camera);
+    return camera;
+  }
 
   public:
   CameraPool()
@@ -37,7 +73,10 @@ class CameraPool
           cam.read(testFrame);
           cameras.push_back(i);
           // cameras.push_back(cam);
-          lastframe[i] = "";
+          std::vector<unsigned char> buffer;
+          cv::imencode(".jpg", testFrame, buffer, std::vector<int>());
+          string encoded = std::string(buffer.begin(), buffer.end());
+          lastframe[cam] = encoded;
         }
       }
       catch(...)
@@ -53,10 +92,10 @@ class CameraPool
     {
       try
       {
-        auto camera = VideoCapture(cameras[cam]);
+        Camera camera = getCam(cameras[cam]);
         //read and encode frame
         Mat frame;
-        camera.read(frame);
+        camera.cam->read(frame);
         // cameras[cam].read(frame);
         std::vector<unsigned char> buffer;
         cv::imencode(".jpg", frame, buffer, std::vector<int>());
@@ -67,26 +106,6 @@ class CameraPool
       {
         cout<<"error reading camera: "<<cameras[cam]<<"\n";
         cout<<e.what()<<"\n";
-        cout<<"attempting restart...\n";
-        try
-        {
-          auto camera = VideoCapture(cameras[cam]);
-          // cameras[cam] = VideoCapture(cam);
-          if(camera.isOpened())
-          {
-            cout<<"restarted\n";
-            Mat frame;
-            // cameras[cam].read(frame);
-            camera.read(frame);
-            std::vector<unsigned char> buffer;
-            cv::imencode(".jpg", frame, buffer, std::vector<int>());
-            encoded = std::string(buffer.begin(), buffer.end());
-            lastframe[cam] = encoded;
-          }
-
-        }
-        catch(...)
-        {cout<<"doublefail\n";}
       }
     }
 
